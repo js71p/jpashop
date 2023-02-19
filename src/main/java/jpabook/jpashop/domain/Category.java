5606d9b0d7c829ccbe25f5e3df49a4839053841c
ryujisoo <croso071@naver.com>
2023-02-19 19:24:41 +0900

    2023-02-19 연관관계 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"), //내가조인
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID") //반대쪽 조인
+    )
+    private List<Item> items = new ArrayList<>();
+}
