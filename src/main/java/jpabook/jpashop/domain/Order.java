0841aba6a9a8722f98c33cec1cd560c3342bda2b
ryujisoo <croso071@naver.com>
2023-02-20 21:38:49 +0900

    2023-02-20 상속관계 매핑

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "ORDERS") //예약어는 X
-public class Order {
+public class Order extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
